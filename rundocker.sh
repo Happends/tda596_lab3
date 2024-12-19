4f7de111e57f2b1df75a966e40a62fe772356eeb
Happends <a.helmersson@outlook.com>
2024-12-19 06:19:00 +0100

    certificates changed

--- rundocker.sh
+++ rundocker.sh
@@ -1,7 +1,7 @@
-
-port=$1
-ja=$2
-jp=$3
+ip=$1
+port=$2
+ja=$3
+jp=$4
 
 echo "port" $port
 echo "ja" $ja
@@ -10,14 +10,14 @@ echo "jp" $jp
 # bash ./keygen.sh
 
 
-ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -n 1)
+# ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -n 1)
 
 
 #  --progress=plain --no-cache
 
 if [ -z "$jp" ] | [ -z "$ja" ]; 
-then sudo docker build -t chord_server --build-arg PORT=$(echo $port) --build-arg IP="0.0.0.0" .; echo "no join";
-else sudo docker build -t chord_server --build-arg IP="0.0.0.0" --build-arg PORT="$port" --build-arg JIP="$ja" --build-arg JPORT="$jp" .; echo "join"; fi
+then sudo docker build -t chord_server --build-arg PORT=$(echo $port) --build-arg IP="$ip" .; echo "no join";
+else sudo docker build -t chord_server --build-arg IP="$ip" --build-arg PORT="$port" --build-arg JIP="$ja" --build-arg JPORT="$jp" .; echo "join"; fi
 
 echo "ip:" "${ip}"
 echo "port:" "${port}"
